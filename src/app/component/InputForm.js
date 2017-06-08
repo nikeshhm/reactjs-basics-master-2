ac9adc6231438bc20b804e68b56ca3433d3bfb56
nikeshhm <nikeshhm10@gmail.com>
2017-06-08 20:00:31 +0530

    react todo changes

--- src/app/component/InputForm.js
+++ src/app/component/InputForm.js
@@ -4,16 +4,19 @@ export class InputForm extends React.Component{
     constructor(){
         super()
     }
-    onCreateNewItem(){
-        this.props.createItem(this.refs.title.value)
-        this.refs.title.value = ' '
+    onCreateNewItem(event){
+       event.preventDefault();
+        if(this.refs.name.value !== ' '){
+            this.props.createItem(this.refs.name.value)
+            this.refs.name.value = ' '
+        }
     }
     render(){
         return(
             <div>
                <form onSubmit={this.onCreateNewItem.bind(this)}>
                     <div className="form-group formList">
-                        <input type="text" className="form-control" ref="title" placeholder="Enter text" />
+                        <input type="text" className="form-control" ref="name" placeholder="Enter text" />
                     </div>
                     <button type="submit" className="btn btn-default">Create</button>
                 </form>
--- src/app/index.js
+++ src/app/index.js
@@ -10,38 +10,76 @@ class App extends React.Component{
         super()
         this.state = {
             data:[
-                {title:'game 1'},
-                {title:'game 2'},
+
             ]
         }
 
     }
-    onCreate(title){
-        this.state.data.push({
-            title
-        })
+    onCreate(name){
+        var self = this
+        axios.post('https://hub1.eiris.in/testrest/api/', {
+            name,
+        }).then(function (response) {
+            self.onAddStateUpdate(response.data.result)
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
+    onAddStateUpdate(value){
+        this.state.data.push(value);
         this.setState({data:this.state.data})
     }
     onSaveBtn(newValue,oldValue){
-        let getTodo = _.find(this.state.data,(item)=>item.title === oldValue)
-        getTodo.title = newValue;
+        let getTodo = _.find(this.state.data,(item)=>item._id === oldValue._id)
+        getTodo.name = newValue;
         this.setState({
             data:this.state.data
         })
+        axios.patch('https://hub1.eiris.in/testrest/api/', oldValue)
+            .then(function (response) {
+                console.log(response)
+            }).catch(function (error) {
+            console.log(error);
+        });
     }
     onRemoveHandler(value){
-         _.remove(this.state.data,(item)=>item.title === value)
+         _.remove(this.state.data,(item)=>item._id === value._id)
         this.setState({
             data:this.state.data
-        })
+        });
+        axios.delete('https://hub1.eiris.in/testrest/api/', {params: value})
+            .then(function (response) {
+                console.log(response)
+            }).catch(function (error) {
+                console.log(error);
+             });
+    }
+    componentWillMount(){
+        let self = this
+        axios.get('https://hub1.eiris.in/testrest/api/')
+            .then(function (response) {
+                console.log(response.data.result);
+                self.setState({
+                    data:response.data.result
+                })
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
     render(){
         return(
           <div className="col-md-6 col-md-offset-3">
              <InputForm createItem={this.onCreate.bind(this)}/>
-              <ul className="list-group">
-                  {this.state.data.map((item,index)=><ListItem item={item} key={index} saveChange = {this.onSaveBtn.bind(this)} remove={this.onRemoveHandler.bind(this)}/>)}
+              <ul className="list-group todo-list">
+                  {this.state.data.map((item,index)=><ListItem
+                      item={item}
+                      key={index}
+                      saveChange = {this.onSaveBtn.bind(this)}
+                      remove={this.onRemoveHandler.bind(this)}
+                  />)}
               </ul>
+
           </div>
         )
     }
